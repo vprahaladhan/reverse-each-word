dc3b88c5b4df55bd78cfe70748d8f366204a7265
prahal <vprahaladhan@gmail.com>
2021-05-04 10:30:01 +1000

    commit tree 8ba5b25ae71018a0c350a0359bed5f3db1a23596

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
-def reverse_each_word(sentence)
-  split_sentence(sentence).join(" ")
-end
-
-def split_sentence(sentence) 
-  sentence.split(" ").collect!() do |word|
+def reverse_each_word(sentence) 
+  words = sentence.split(" ") 
+  words.collect!() do |word|
     word.reverse()
   end
+  words.join(" ")
 end
-
-puts split_sentence("Hello there, how are you?")
