c705c7b5ac10b71e3dbb574d64cf7d3c8468d869
prahal <vprahaladhan@gmail.com>
2021-05-04 10:26:06 +1000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  split_sentence(sentence).join(" ")
+end
+
+def split_sentence(sentence) 
+  sentence.split(" ").collect() do |word|
+    word.reverse()
+  end
+end
